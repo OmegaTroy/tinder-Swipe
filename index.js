d9435556aaff4f7bfdc6fc91aeb4fdc8e0ef0ef6
OmegaTroy <juli.mjoi44@gmail.com>
2024-09-24 00:09:46 -0300

    new create

--- index.js
+++ index.js
@@ -1,67 +1,83 @@
-const DECISION_THRESHOLD = 75;
-let isAnimating = false;
-let pullDeltaX = 0;
+const DECISION_THRESHOLD = 75
 
-function starDrag(event) {
-  if (isAnimating) return;
-  const actualCard = event.target.closest("article");
-  if (!actualCard) return;
+let isAnimation = false
+let pullDeltaX = 0
 
-  const startX = event.pageX ?? event.touches[0].pageX;
-  document.addEventListener("mousemove", onMove);
-  document.addEventListener("mouseup", onEnd);
-  document.addEventListener("touchmove", onMove, { passive: true });
-  document.addEventListener("touchend", onMove, { passive: true });
+function startDrag(event){
+  if(isAnimation) return
 
-  function onMove(event) {
-    const currentX = event.pageX ?? event.touches[0].pageX;
-    pullDeltaX = currentX - startX;
-    if (pullDeltaX === 0) return;
+  const actualCard = event.target.closest('article')
+  if(!actualCard) return
 
-    isAnimating = true;
-    const deg = pullDeltaX / 14;
+  const startX = event.pageX ?? event.touches[0].pageX
 
-    actualCard.style.transform = `translateX(${pullDeltaX}px) rotate(${deg}deg)`;
+  document.addEventListener('mousemove', onMove)
+  document.addEventListener('mouseup', onEnd)
 
-    const opacity = Math.abs(pullDeltaX) / 100;
-    isRight = pullDeltaX > 0;
+  document.addEventListener('touchmove',onMove, {passive:true})
+  document.addEventListener('touchend',onEnd, {passive: true })
+
+  function onMove(event){
+    const currentX = event.pageX ?? event.touches[0].pageX
+
+    pullDeltaX = currentX - startX
+
+    if(pullDeltaX === 0) return
+
+    isAnimation = true
+
+    const deg = pullDeltaX / 14
+
+    actualCard.style.transform = `translateX(${pullDeltaX}px) rotate(${deg}deg)`
+
+    actualCard.style.cursor = 'grabbing'
+    const opacity = Math.abs(pullDeltaX) / 100
+    const isRight = pullDeltaX > 0
 
     const choiceEl = isRight
-      ? actualCard.querySelector(".choice.like")
-      : actualCard.querySelector(".choice.nope");
-    choiceEl.style.opacity = opacity;
+    ? actualCard.querySelector('.choice.like')
+    : actualCard.querySelector('.choice.nope')
+
+    choiceEl.style.opacity = opacity
   }
 
-  function onEnd(event) {
-    document.removeEventListener("mousemove", onMove);
-    document.removeEventListener("mouseend", onEnd);
-    document.removeEventListener("touchmove", onMove);
-    document.removeEventListener("touchend", onEnd);
+  function onEnd(event){
+    document.removeEventListener('mousemove', onMove)
+    document.removeEventListener('mouseup', onEnd)
 
-    const decisionMade = Math.abs(pullDeltaX) >= DECISION_THRESHOLD;
-    if (decisionMade) {
-      const goRight = pullDeltaX >= 0;
-      actualCard.classList.add(goRight ? "go-right" : "go-left");
-      actualCard.addEventListener("transitionend", () => {
-        actualCard.remove();
-      });
-    } else {
-      actualCard.classList.add("reset");
-      actualCard.classList.remove("go-right", "go-left");
-      actualCard.querySelectorAll(".choice").forEach((choice) => {
-        choice.style.opacity = 0;
-      });
+    document.removeEventListener('touchmove', onMove)
+    document.removeEventListener('touchend', onEnd)
+
+    const decisionMade = Math.abs(pullDeltaX) >= DECISION_THRESHOLD
+    if(decisionMade){
+      const goRight = pullDeltaX >= 0
+
+      actualCard.classList.add(goRight ? 'go-right' : 'go-left')
+      actualCard.addEventListener('transitionend',()=>{
+        actualCard.remove()
+      })
+    }else{
+      actualCard.classList.add('reset')
+      actualCard.classList.remove('go-right', 'go-left')
+      actualCard.querySelectorAll('.choice').forEach(choice => {
+        choice.style.opacity = 0
+      })
     }
-    actualCard.addEventListener("transitionend", () => {
-      actualCard.removeAttribute("style");
-      actualCard.classList.remove("reset");
-      pullDeltaX = 0;
-      isAnimating = false;
-    });
-    actualCard
-      .querySelectorAll(".choice")
-      .forEach((el) => (el.style.opacity = 0));
   }
+
+  actualCard.addEventListener('transitionend',()=>{
+    actualCard.removeAttribute('style')
+    actualCard.classList.remove('reset')
+
+    pullDeltaX = 0 
+    isAnimation = false
+  })
+
+  actualCard
+  .querySelectorAll('.choice')
+  .forEach((el)=>(el.style.opacity =0))
 }
-document.addEventListener("mousedown", starDrag);
-document.addEventListener("touchstart", starDrag, { passive: true });
+
+
+document.addEventListener('mousedown',startDrag)
+document.addEventListener('touchstart',startDrag,{ passive: true })
